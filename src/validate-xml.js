5b7bf24d5c167c78a78d83abe1a9e40f8ab4d3a5
Mo Zaqout <mo.zaqout@gmail.com>
2021-06-09 19:24:34 +0300

    Validate xml file against schema,  using xsd-schema-validator

--- src/validate-xml.js
+++ src/validate-xml.js
@@ -1,4 +1,10 @@
 const XML_DATA_PATH = './resources/data.xml'
-const XML_DATA_SCHEMA = './resources/data.xsd'
-// TODO: XML, loading data
-// TODO: XML, validate data
+const XML_DATA_SCHEMA_PATH = './resources/data.schema.xsd'
+
+const validator = require('xsd-schema-validator');
+
+// TODO: validate xml file
+validator.validateXML({file: XML_DATA_PATH}, XML_DATA_SCHEMA_PATH,
+    function (err, result) {
+        console.log(result.valid)
+    });
